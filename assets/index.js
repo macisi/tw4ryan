ddbd91ba43e94cef109e08e54c7f819ee00dfd82
biangang.bg <biangang.bg@alibaba-inc.com>
2015-12-04 17:17:50 +0800

    add files

---
+++ assets/index.js
@@ -0,0 +1,4 @@
+$.ajax({
+	url: 'https://api.twitter.com/1.1/statuses/home_timeline.json',
+	dataType: 'jsonp'
+});
--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const koa = require('koa');
 const router = require('koa-router')();
 const mount = require('koa-mount');
 const session = require('koa-session');
+const views = require('koa-views');
 const Grant = require('grant-koa');
+const serve = require('koa-static');
 
 const grant = new Grant({
 	server: {
@@ -24,16 +26,20 @@ app.keys = ['grant'];
 
 router
 	.get('/', function *(next){
-		this.body = 'hello buddy!';
+		yield this.render('index');
 	})
 	.get('/callback', function *(next){
-	this.body = JSON.stringify(this.query);
-});
-
+		this.body = JSON.stringify(this.query);
+	});
+	
 app
+	.use(serve(__dirname + '/assets'))
+	.use(views(__dirname + '/view', {
+		default: 'jade'
+	}))
 	.use(session(app))
 	.use(mount(grant))
-	.use(router.routes())
+	.use(router.routes());
 
 app.listen(8082, function(){
 	console.log('Koa server listening on port 8082');
